af44136f310dfc104c315f2c5f2dbe3a70fbce9b
yubin.park <qkrdbqls1001@naver.com>
2022-06-22 14:59:25 +0900

    feat: Body-Parser, Mongoose dependendy 추가
    + boilerplate code

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3200;
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Mongoose
+const MONGO_URI = process.env.MONGO_URI;
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB is successfully connected."))
+    .catch((e) => console.log(e));
 
 app.get("/", (req, res, next) => {
     res.send("Hello, World!");
