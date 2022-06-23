fb3f564f2e125c9bd16dc0a77ee18348d9383d3f
yubin.park <qkrdbqls1001@naver.com>
2022-06-23 12:57:21 +0900

    feat(auth): authJWT middleware에서 인증된 사용자를 확인할 수 있도록 처리

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 const jwt = require("../util/jwt");
 
+/*
+로그인을 수행하는 뷰
+*/
 exports.loginView = function (req, res, next) {
     const username = req.body.username;
     const password = req.body.password;
@@ -13,17 +16,17 @@ exports.loginView = function (req, res, next) {
 
     User.findOne({ username: username }, (err, user) => {
         if (err) {
-            return next(err);
+            next(err);
         }
 
         if (user) {
             user.checkPassword(password, function (err, isMatch) {
                 if (err) {
-                    return done(err);
+                    done(err);
                 }
 
                 if (isMatch) {
-                    const accessToken = jwt.sign({ id: user._id });
+                    const accessToken = jwt.sign(user);
                     res.status(200).json({ token: accessToken });
                 } else {
                     res.status(401).send();
@@ -35,6 +38,9 @@ exports.loginView = function (req, res, next) {
     });
 };
 
+/*
+회원 가입을 수행하는 뷰
+*/
 exports.signUpView = function (req, res, next) {
     const username = req.body.username;
     const password = req.body.password;
@@ -47,7 +53,7 @@ exports.signUpView = function (req, res, next) {
 
     User.findOne({ username: username }, (err, user) => {
         if (err) {
-            return next(err);
+            next(err);
         }
 
         if (user) {
@@ -65,6 +71,14 @@ exports.signUpView = function (req, res, next) {
     });
 };
 
-exports.authCheckView = function (req, res, next) {
-    res.status(200).json({ id: req.id });
+/*
+JWT 테스트용 뷰
+외부에서 해당 View에 접근하기 이전에 JWT verify를 거치게 했으므로
+해당 API에 대해 200 status code를 반환 받음은 jwt 인증 성공을 의미함
+*/
+exports.authCheckView = function (req, res) {
+    res.status(200).json({
+        id: req.id,
+        username: req.user.username,
+    });
 };
--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,5 +1,17 @@
+/*
+    인증 관련 Middleware
+*/
+
+const User = require("../models/user");
 const { verify } = require("../util/jwt");
 
+/*
+    JWT 인증을 수행하는 middleware로,
+    1. JWT가 'Bearer {{ JWT }}'의 형태로 Authorization header에 전달되지 않은 경우
+    2. JWT decoding 중 문제가 발생한 경우 (유효하지 않은 token)
+    3. 만료된 JWT인 경우
+    위 세 가지 경우 401 status code를 반환하도록 함
+*/
 const authJWT = (req, res, next) => {
     if (req.headers.authorization) {
         const token = req.headers.authorization.split("Bearer ")[1];
@@ -7,7 +19,16 @@ const authJWT = (req, res, next) => {
 
         if (result.ok) {
             req.id = result.id;
-            next();
+            User.findOne({ id: req.id }, (err, user) => {
+                if (err) {
+                    next(err);
+                } else {
+                    // JWT가 유효함이 검증되면 req 객체에
+                    // 해당 JWT를 생성한 사용자를 속성으로 추가함 (View에서 받아 쓰도록)
+                    req.user = user;
+                    next();
+                }
+            });
         } else {
             res.status(401).send({
                 ok: false,
@@ -17,6 +38,8 @@ const authJWT = (req, res, next) => {
     } else {
         res.status(401).json({ message: "No JWT" });
     }
+
+    return;
 };
 
 module.exports = authJWT;
