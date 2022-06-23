4505191529e6cfbcc33e34017e596b2124032df3
yubin.park <qkrdbqls1001@naver.com>
2022-06-23 10:20:37 +0900

    fix(auth): :bug: 누락된 controller 연결 코드 추가

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -4,9 +4,8 @@ const controller = require("../controllers/authentication");
 
 router.get("/login", controller.loginView);
 router.post("/signup", controller.signUpView);
-router.use("/auth-test", controller);
 
 const jwtMiddleware = require("../middlewares/authentication");
-router.get("/test", jwtMiddleware, controller.authCheckView);
+router.use("/test", jwtMiddleware, controller.authCheckView);
 
 module.exports = router;
